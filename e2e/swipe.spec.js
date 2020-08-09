dff705276cf5beb2bc631c08e1635975f6805cc7
Marius Reimer <reime005@gmail.com>
2020-08-09 12:03:43 +0200

    test

--- e2e/swipe.spec.js
+++ e2e/swipe.spec.js
@@ -37,10 +37,10 @@ describe('Test App', () => {
     await expect(element(by.id('flatlist_modal_view'))).toBeVisible();
     await expect(element(by.id('pull_down_bar'))).toBeVisible();
 
-    await element(by.id('image_scroll_view')).swipe('down', 'fast', 0.5);
+    await element(by.id('image_scroll_modal')).swipe('down', 'fast', 1);
 
     await waitFor(element(by.id('image_scroll_view')))
       .toBeNotVisible()
-      .withTimeout(2000);
+      .withTimeout(4000);
   });
 });
